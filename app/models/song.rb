786f847ad030070af08851d26be4e21bb8d55072
Chris Gullickson <cgullickson5@gmail.com>
2018-02-08 17:24:27 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,8 @@
 class Song < ActiveRecord::Base
   validates_presence_of :title
   validates :title, uniqueness: [:artist_name, :release_year]
+  validates_presence_of :release_year, if: :released?
   validates :released, inclusion: {in: [true, false]}
   validates_presence_of :artist_name
+  
 end

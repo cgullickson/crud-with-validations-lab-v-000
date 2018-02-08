b096f60ff9378e2892d2571b1b557b596f3836cf
Chris Gullickson <cgullickson5@gmail.com>
2018-02-08 17:31:21 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,8 +6,4 @@ class Song < ActiveRecord::Base
   validates_presence_of :artist_name
   validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, if: :released
 
-  def released?
-    released
-  end
-
 end
